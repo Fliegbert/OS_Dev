8bdeec1b6d6e89355bfa429fe4945e5cef863082
Dalvin-Ehinoma Noah Aiguobas <pharcodra@gmail.com>
2023-11-05 11:45:57 +0100

    added vesa, needs refactor but works

--- boot_info/bios_info.asm
+++ boot_info/bios_info.asm
@@ -7,11 +7,16 @@ call detect_memory
 mov si, mem_call_success
 call print
 
+call vesa_start
+mov si, vesa_call_success
+call print
 
 jmp halt
 
 %include "boot_info/include/memory_detection.inc"
+%include "boot_info/include/vesa.inc"
 
 welcome_lbl		db 'Welcome to Sector 2', ENDL, 0
 mem_call_success	db "detect Memory success", ENDL, 0
+vesa_call_success	db "vesa success", ENDL, 0
 times 512		db 0
